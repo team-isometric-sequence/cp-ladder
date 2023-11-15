68ce47ab1a092012d2da2ff2fa4c2993cc9cc2c0
malkoG <rijgndqw012@gmail.com>
2023-11-15 16:52:48 +0900

    Setup django-tailwind in order to speed up publishing

--- memedex/settings.py
+++ memedex/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'tailwind',
+    'memedex_theme',
+    'django_browser_reload',
+
+
     'core',
 ]
+
+TAILWIND_APP_NAME = 'memedex_theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'memedex.urls'
@@ -122,3 +131,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
---
+++ memedex_theme/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Memedex_themeConfig(AppConfig):
+    name = 'memedex_theme'
