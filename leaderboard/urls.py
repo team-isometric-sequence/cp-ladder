d10a2e3d2ec33acd22897e4c69c8581f8d792103
malkoG <rijgndqw012@gmail.com>
2024-02-28 03:18:50 +0900

    Add link to each school's leaderboard page in SchoolSelection page

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path
 
-from leaderboard.views import SchoolSelectionView
+from leaderboard.views import SchoolLeaderboardView, SchoolSelectionView
 
 
 urlpatterns = [ 
-    path('', SchoolSelectionView.as_view())
+    path('', SchoolSelectionView.as_view()),
+    path('leaderboard/<str:school>', SchoolLeaderboardView.as_view(), name="school-leaderboard")
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,5 +1,16 @@
 from django.views.generic.base import TemplateView
 
+
 # Create your models here.
 class SchoolSelectionView(TemplateView):
     template_name = "leaderboard/school_selection.html"
+
+class SchoolLeaderboardView(TemplateView):
+    template_name = "leaderboard/school_leaderboard.html"
+
+    def get_context_data(self, **kwargs):
+        school = self.kwargs.get('school', "I DON'T KNOW")
+        context = { 'school': school }
+        print(context)
+        return context
+
