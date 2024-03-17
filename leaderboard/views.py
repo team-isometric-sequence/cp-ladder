3323183b99494ab528d5a94d2015b6e409fc3903
malkoG <rijgndqw012@gmail.com>
2024-03-17 13:38:38 +0900

    Add pagination and filtering to SchoolLeaderboard view
    - Integrate Paginator for dividing problems into pages
    - Implement problem filtering based on tags, order, and rank eligibility
    - Update context with pagination and query parameters for the template

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,4 +1,7 @@
 from django.views.generic.base import TemplateView
+from django.core.paginator import Paginator
+
+from leaderboard.services.honor_farming_service import list_unsolved_problems, ProblemFilterType
 
 
 # Create your models here.
@@ -10,7 +13,25 @@ class SchoolLeaderboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         school = self.kwargs.get('school', "I DON'T KNOW")
-        context = { 'school': school }
-        print(context)
+        tag_name = self.request.GET.get('tag_name', '')
+        order_by = self.request.GET.get('order_by', 'tier')
+        page_number = int(self.request.GET.get("page", 1))
+        allow_unranked = bool(self.request.GET.get('allow_unranked', False))
+
+
+        query: ProblemFilterType = { 
+            'allow_unranked': allow_unranked,
+            'school': school,
+            'tag_name':  tag_name,
+            'order_by': order_by,
+        }
+
+        problems = list_unsolved_problems(query)
+
+        paginator = Paginator(problems, 50)
+        page_obj = paginator.get_page(page_number)
+
+        context = { 'school': school, 'query': query, 'page_obj': page_obj}
+
         return context
 
