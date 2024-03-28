5f1b41eab8331a996376bc955edbe03690427833
malkoG <rijgndqw012@gmail.com>
2024-03-28 09:36:37 +0900

    Seperate django settings module for different environment

--- cp_ladder/asgi.py
+++ cp_ladder/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp_ladder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cp_ladder.settings.development')
 
 application = get_asgi_application()
